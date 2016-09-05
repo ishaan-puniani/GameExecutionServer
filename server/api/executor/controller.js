d8dff4f6abf7f1fbb5d602cd8cf2e2286b81ca28
Ishaan Puniani <ishaan.puniani@gmail.com>
2016-09-05 09:28:52 +0200

    Updated For New featured

--- server/api/executor/controller.js
+++ server/api/executor/controller.js
@@ -4,10 +4,10 @@ import _ from 'lodash';
 var Slot_Server = require('Slot_Server');
 var JackOrBetter_Server = require('JackOrBetter_Server');
 
-var cache = {}; 
+var cache = {};
 function respondWithResult(res, statusCode) {
   statusCode = statusCode || 200;
-  return function(entity) {
+  return function (entity) {
     if (entity) {
       res.status(statusCode).json(entity);
     }
@@ -16,45 +16,49 @@ function respondWithResult(res, statusCode) {
 
 
 /*
-function handleEntityNotFound(res) {
-  return function(entity) {
-    if (!entity) {
-      res.status(404).end();
-      return null;
-    }
-    return entity;
-  };
-}
+ function handleEntityNotFound(res) {
+ return function(entity) {
+ if (!entity) {
+ res.status(404).end();
+ return null;
+ }
+ return entity;
+ };
+ }
 
-function handleError(res, statusCode) {
-  statusCode = statusCode || 500;
-  return function(err) {
-    res.status(statusCode).send(err);
-  };
-}
-*/
+ function handleError(res, statusCode) {
+ statusCode = statusCode || 500;
+ return function(err) {
+ res.status(statusCode).send(err);
+ };
+ }
+ */
 export function getCachedGames(req, res) {
-  respondWithResult(res,200)(cache);  
+  respondWithResult(res, 200)(cache);
 }
 
-export function execute(req, res) { console.log(req.body);
-    var game = req.body.game,
+export function execute(req, res) {
+  console.log(req.body);
+  var game = req.body.game,
     action = req.body.action;
-   
-    var params = {
-        bet:parseFloat(req.body.bet),
-        betLevel:req.body.betLevel ? parseFloat(req.body.betLevel):0,
-        holdedCards:req.body.holdedCards,
-        betLevelIdx : req.body.betLevelIdx ? parseFloat(req.body.betLevelIdx):0
-    };
-    console.log("GEE : execute",game,action);
-    console.log(req.body);
-    if(!cache[game]){
-        cache[game] = require(game); 
-    }
-    console.log(game,action,params);
-    var gameResponse = cache[game][action](params);
-    respondWithResult(res,200)(gameResponse);  
-  
+
+  var params = {
+    bet: parseFloat(req.body.bet),
+    betLevel: req.body.betLevel ? parseFloat(req.body.betLevel) : 0,
+    holdedCards: req.body.holdedCards,
+    betLevelIdx: req.body.betLevelIdx ? parseFloat(req.body.betLevelIdx) : 0,
+    isRestore: req.body.isRestore || false,
+    restoredData: req.body.restoredData,
+    cheat: req.body.cheat
+  };
+  console.log("GEE : execute", game, action);
+  console.log(req.body);
+  if (!cache[game]) {
+    cache[game] = require(game);
+  }
+  console.log(game, action, params);
+  var gameResponse = cache[game][action](params);
+  respondWithResult(res, 200)(gameResponse);
+
 }
 
