93b9de2596287e270acf35cbf13ddc7e5752f45e
Ishaan Puniani <ishaan.puniani@gmail.com>
2016-09-07 17:34:52 +0200

    Fix test

--- server/api/executor/controller.integration.js
+++ server/api/executor/controller.integration.js
@@ -4,59 +4,61 @@ var app = require('../..');
 import request from 'supertest';
 
 
-describe('Executor API:', function() {
+describe('Executor API:', function () {
 
-  describe('POST init /api/execute', function() {
-    var things;
-    beforeEach(function(done) {
-      request(app)
-        .post('/api/execute')
-        .send({
-          game: 'Slot_Server',
-          action: 'init'
-        })
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .end((err, res) => {
-          if (err) {
-            return done(err);
-          }
-          things = res.body;
-          done();
+    describe('POST init /api/execute', function () {
+        var things;
+        beforeEach(function (done) {
+            request(app)
+                .post('/api/execute')
+                .send({
+                    game: 'Slot_Server',
+                    action: 'init'
+                })
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+                    things = res.body;
+                    done();
+                });
         });
-    });
 
-     it('should respond with JSON', function() {
-      expect(things).to.be.instanceOf(Object);
-    });
-
-  });
-  
-    describe('POST spin /api/execute', function() {
-	  var things;
-    beforeEach(function(done) {
-      request(app)
-        .post('/api/execute')
-        .send({
-          game: 'Slot_Server',
-          action: 'spin'
-        })
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .end((err, res) => {
-          if (err) {
-            return done(err);
-          }
-          things = res.body;
-          done();
+        it('should respond with JSON', function () {
+            expect(things).to.be.instanceOf(Object);
         });
+
     });
 
-    it('should respond with JSON', function() {
-      expect(things).to.be.instanceOf(Object);
+    describe('POST spin /api/execute', function () {
+        var things;
+        beforeEach(function (done) {
+            request(app)
+                .post('/api/execute')
+                .send({
+                    bet: 0,
+                    game: 'Slot_Server',
+                    action: 'spin'
+                })
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+                    console.log("Response of spin", res.body);
+                    things = res.body;
+                    done();
+                });
+        });
+
+        it('should respond with JSON', function () {
+            expect(things).to.be.instanceOf(Object);
+        });
+
     });
 
-  });
-  
-  
+
 });
